951a26c66552ba24df68ae55b388138aef357c54
s-armaan <144650842+s-armaan@users.noreply.github.com>
2026-08-28 19:48:15 -0700

    feat: add uncaught exception handler to main method to show the user a dialog

--- src/main/java/doc_router/Main.java
+++ src/main/java/doc_router/Main.java
@@ -1,9 +1,23 @@
 package doc_router;
 
-public final class Main {
-    private Main() {
-    }
+import java.nio.file.Path;
 
+import javax.swing.JOptionPane;
+
+import doc_router.util.Config;
+
+public final class Main {
     public static void main(String[] args) {
+        Thread.setDefaultUncaughtExceptionHandler((thread, error) -> {
+            error.printStackTrace();
+
+            JOptionPane.showMessageDialog(
+                    null,
+                    "Unexpected error:\n" + error.getMessage(),
+                    "Doc Router error",
+                    JOptionPane.ERROR_MESSAGE);
+
+            System.exit(1);
+        });
     }
 }
